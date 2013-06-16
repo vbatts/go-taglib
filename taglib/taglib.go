fb5847203143fe349159b2390a52d4a550def1f7
Vincent Batts <vbatts@redhat.com>
2013-06-16 10:52:34 -0400

    Adding comments and fixing a memory leak on libtag_c freeing char*

--- taglib/taglib.go
+++ taglib/taglib.go
@@ -8,7 +8,7 @@ import "unsafe"
 
 type Tags struct {
 	Title, Artist, Album, Comment, Genre string
-	Year, Track int
+	Year, Track                          int
 }
 
 type Properties struct {
@@ -23,54 +23,70 @@ func Open(filename string) *File {
 	return (*File)(C.taglib_file_new(fp))
 }
 
-func (f *File)Close() {
+func (f *File) Close() {
 	C.taglib_file_free((*C.TagLib_File)(f))
 }
 
-func (f *File)GetTags() *Tags {
+/*
+Get the ID3 taglib.Tags from this taglib.File
+*/
+func (f *File) GetTags() *Tags {
 	ts := C.taglib_file_tag((*C.TagLib_File)(f))
 
 	a := Tags{}
 
-	if ts!=nil {
-		a.Title   = C.GoString(C.taglib_tag_title(ts))
-		a.Artist  = C.GoString(C.taglib_tag_artist(ts))
-		a.Album   = C.GoString(C.taglib_tag_album(ts))
+	if ts != nil {
+		a.Title = C.GoString(C.taglib_tag_title(ts))
+		a.Artist = C.GoString(C.taglib_tag_artist(ts))
+		a.Album = C.GoString(C.taglib_tag_album(ts))
 		a.Comment = C.GoString(C.taglib_tag_comment(ts))
-		a.Genre   = C.GoString(C.taglib_tag_genre(ts))
-		a.Year    = int(C.taglib_tag_year(ts))
-		a.Track   = int(C.taglib_tag_track(ts))
+		a.Genre = C.GoString(C.taglib_tag_genre(ts))
+		a.Year = int(C.taglib_tag_year(ts))
+		a.Track = int(C.taglib_tag_track(ts))
 	}
+
+	defer C.taglib_tag_free_strings()
 	return &a
 }
 
-func (f *File)GetProperties() *Properties {
+/*
+Get the taglib.Properties from this taglib.File
+*/
+func (f *File) GetProperties() *Properties {
 	ap := C.taglib_file_audioproperties((*C.TagLib_File)(f))
-	if ap==nil { return nil }
+	if ap == nil {
+		return nil
+	}
 
 	p := Properties{}
-	p.Length     = int(C.taglib_audioproperties_length(ap))
-	p.Bitrate    = int(C.taglib_audioproperties_bitrate(ap))
+	p.Length = int(C.taglib_audioproperties_length(ap))
+	p.Bitrate = int(C.taglib_audioproperties_bitrate(ap))
 	p.Samplerate = int(C.taglib_audioproperties_samplerate(ap))
-	p.Channels   = int(C.taglib_audioproperties_channels(ap))
+	p.Channels = int(C.taglib_audioproperties_channels(ap))
 
+	defer C.taglib_tag_free_strings()
 	return &p
 }
 
-
+/*
+Get the ID3 taglib.Tags from filename
+*/
 func GetTags(filename string) *Tags {
 	tf := Open(filename)
-	if tf==nil { 
-		return nil 
+	if tf == nil {
+		return nil
 	}
 	defer tf.Close()
 	return tf.GetTags()
 }
 
+/*
+Get the taglib.Properties from filename
+*/
 func GetProperties(filename string) *Properties {
 	tf := Open(filename)
-	if tf==nil { 
-		return nil 
+	if tf == nil {
+		return nil
 	}
 	defer tf.Close()
 	return tf.GetProperties()
